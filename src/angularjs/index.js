d33d04dd344d640db9f337bee2103c80369aac75
yotaiyo <yota071218@gmail.com>
2020-01-13 16:54:39 +0900

    angularjsフォルダを作成

---
+++ src/angularjs/index.js
@@ -0,0 +1,16 @@
+const app = angular.module('todoApp', []);
+
+app.controller('todoAppCtrl', function($scope) {
+    $scope.todoList = []
+    $scope.addTodo = function(name) {
+        $scope.todoList.push({ name, done: false })
+        $scope.textInput = []
+    }
+    $scope.deleteTodo = function(index) {
+        $scope.todoList.splice(index, 1)
+    }
+    $scope.completeTodo = function(index) {
+        const todo = $scope.todoList[index]
+        $scope.todoList[index] = { ...todo, done: !todo.done }
+    }
+});
