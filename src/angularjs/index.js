6f50b3e9100d490e7bb9d9336d757c8673f2742b
yotaiyo <yota071218@gmail.com>
2020-01-13 21:46:10 +0900

    余計なコードを削除

--- src/angularjs/index.js
+++ src/angularjs/index.js
@@ -1,16 +1,1 @@
 const app = angular.module('todoApp', ['react']);
-
-app.controller('todoAppCtrl', function($scope) {
-    $scope.todoList = []
-    $scope.addTodo = function(name) {
-        $scope.todoList.push({ name, done: false })
-        $scope.textInput = []
-    }
-    $scope.deleteTodo = function(index) {
-        $scope.todoList.splice(index, 1)
-    }
-    $scope.completeTodo = function(index) {
-        const todo = $scope.todoList[index]
-        $scope.todoList[index] = { ...todo, done: !todo.done }
-    }
-});
