356a7307a26d4ed3b35ed4126bc0c0a90bd85633
yotaiyo <yota071218@gmail.com>
2020-01-13 21:44:25 +0900

    todoアプリの雛形をreact componentで作成

--- src/react/index.jsx
+++ src/react/index.jsx
@@ -1,7 +1,33 @@
-class Test extends React.Component {
+class Todo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { todoList: [{ name: 'post1' }, { name: 'post2' }], textInput: '' };
+    }
+
+    onChange(textInput) {
+        this.setState({ textInput })
+    }
+
     render() {
+        const { todoList, textInput } = this.state
         return (
-            <span>Test</span>
+            <div>
+                <input type='text' value={this.state.textInput} onChange={e => this.onChange(e.target.value)} />
+                <button>add</button>
+                <ul>
+                    {todoList.map((todo, index) => (
+                        <li key={index}>
+                            <span style={{ lineDecoration: 'line-through' }}>
+                                {todo.name}
+                            </span>
+                            <button>delete</button>
+                            <button>done</button>
+                        </li>
+
+                    ))
+                    }
+                </ul>
+            </div>
         )
     }
 }
